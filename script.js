a86db459ada1935374a71ca0a7ac3e9e2b6a58d7
URMIK J PATEL <114993425+PRABHU-ujp6@users.noreply.github.com>
2023-12-27 11:28:43 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function login(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var loginMessage = document.getElementById("login-message").value;
+
+    if(username ===  'ujp' && password === "1234"){
+        loginMessage.innerHTML = "Login Succesfull !";
+        loginMessage.style.color = "green";
+    }
+    else{
+        loginMessage.innerHTML = "Login Failed. Please Check Your Username and Password...";
+        loginMessage.style.color = "red";
+    }
+
+    alert(`Login  Successfully\nUsernamr: ${username}\nPassword: ${password}\nloginMessage: ${login-message}`);
+
+
+}
